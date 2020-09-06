e8b2a9092995252ecf7012f6145357585acec0b1
y2kmz <mou.shin.tokyo@gmail.com>
2020-09-07 01:07:12 +0900

    pointnet代码，升级至适用于TF2.1和python3.7，加入大量用于学习的注释。 date文件夹中需要下载下面的文件，并直接解压在data文件夹中。 https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip

--- provider.py
+++ provider.py
@@ -13,6 +13,7 @@ DATA_DIR = os.path.join(BASE_DIR, 'data')
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 # 若不存在指定的文件夹，则从指定url下载压缩包，并解压缩
+# 实际上不好用，zipfile下载不下来。所以mv和rm就都报错了。
 if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
     www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
     zipfile = os.path.basename(www)
